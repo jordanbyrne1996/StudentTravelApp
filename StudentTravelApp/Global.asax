58d9b863d3cc5c3d2750aed640c9b2c8ea27ccf2
Jordan Byrne <jordan.byrne@mydit.ie>
2018-04-04 16:37:14 +0100

    Add project files.

---
+++ StudentTravelApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StudentTravelApp.MvcApplication" Language="C#" %>
---
+++ StudentTravelApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StudentTravelApp.Startup))]
+namespace StudentTravelApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
